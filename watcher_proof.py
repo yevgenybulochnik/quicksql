09447111a0098ee3c28c154bd51767efaac231e4
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2026-01-12 21:06:08 -0600

    add logging

--- watcher_proof.py
+++ watcher_proof.py
@@ -1,6 +1,16 @@
+import logging
 import time
 from pathlib import Path
 from watchdog.observers import Observer
+
+# Configure logging only for the quicksql package
+logger = logging.getLogger("quicksql")
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
+logger.addHandler(handler)
 
 from quicksql.core.watcher import QsqlFileEventHandler
 
